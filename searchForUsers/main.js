8d4ac7350ec6f9847bc9327e2c87733789679b77
oussama drifi <157324879+oussama-drifi@users.noreply.github.com>
2025-09-15 20:56:24 +0100

    Add files via upload

---
+++ searchForUsers/main.js
@@ -0,0 +1,82 @@
+const input = document.querySelector(".container input");
+const clear = document.getElementById("clear");
+const menu = document.getElementById("menu");
+const spinner = document.querySelector(".loader");
+
+const countries = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+    "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+    "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+    "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica",
+    "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador",
+    "Egypt", "England", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
+    "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
+    "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland",
+    "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait",
+    "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
+    "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico",
+    "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
+    "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman",
+    "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal",
+    "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+    "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+    "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria",
+    "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
+    "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu",
+    "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+];
+
+let handler;
+input.addEventListener("input", () => {
+    // clear the list
+    menu.innerHTML = "";
+
+    // clear the setTimeout before
+    clearTimeout(handler);
+
+    // show / hide the spinner
+    input.value !== "" ? spinner.style.display = "flex" : spinner.style.display = "none";
+
+    // handle clear btn
+    clear.classList.toggle("show", input.value !== "")
+
+    // don't search if there's no input
+    if (input.value === "") {
+        menu.innerHTML = "";
+        return;
+    }
+    // start new setTimeout
+    handler = setTimeout(() => {
+        // hide the spinner
+        spinner.style.display = "none";
+        // handle search
+        const start = input.value.trim().toLocaleUpperCase();
+        const result = countries.filter(elm => elm.toLocaleUpperCase().includes(start));
+        menu.innerHTML = result.map(elm => `<li role="option">${elm}</li>`).join('');
+        menu.querySelectorAll("li").forEach(li => {
+            
+        })
+        // no countries found
+        if (menu.innerHTML === ""){
+            menu.innerHTML = '<li style="pointer-events: none">Oops, No countries found!</li>'
+        }
+    }, 400);
+
+})
+
+clear.onclick = () => {
+    input.value = "";
+    clear.classList.remove("show");
+    menu.innerHTML = "";
+    // hide the spinner
+    spinner.style.display = "none";
+    // clear the setTimeout
+    clearTimeout(handler) // in case type and clear quickly
+}
+
+// debouncing makes you avoid wasteful filtering/re-rendering during fast typing
+// The function inside setTimeout only runs once the user pauses typing for 200ms
+// Use debounce when:
+// ----- You’re filtering large data
+// ----- You’re calling APIs
+// ----- You want to optimize performance or improve UX
